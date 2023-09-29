ec9d5ccb27f3769e84221fdff08785a1e071424e
aidanb2002 <134320069+aidanb2002@users.noreply.github.com>
2023-09-29 13:11:41 -0500

    Update Unique.R

--- Unique.R
+++ Unique.R
@@ -3,51 +3,84 @@ library("readxl")
 library("stringr")
 library(openxlsx)
 
-
-## Download Data and Set working Directory ##
-
+# Set Working Directory
 setwd("/Users/aidanbeilke/Downloads")
-dashboard <- read_xlsx("specs (26).xlsx")
-hard <- read.xlsx("bbe (5).xlsx")
-spin2 <- read.csv("active-spin (3).csv")
-spin3 <- read.csv("active-spin (4).csv")
 
-# Set Adequate Sample Size #
+#Import 2021 Data
+dashboard21 <- read_xlsx("specs2021.xlsx")
+hard21 <- read.xlsx("bbe_2021.xlsx") %>% 
+  mutate(Year = 2021)
+spin21 <- read.csv("active-spin_2021.csv") %>% 
+  mutate(Year = 2021)
 
+#Import 2022 Data
+dashboard22 <- read_xlsx("specs_2022.xlsx")
+hard22 <- read.xlsx("bbe_2022.xlsx") %>% 
+  mutate(Year = 2022)
+spin22 <- read.csv("active-spin_2022.csv") %>% 
+  mutate(Year = 2022)
+
+#Import 2023 Data
+dashboard23 <- read_xlsx("specs_2023.xlsx")
+hard23 <- read.xlsx("bbe_2023.xlsx") %>% 
+  mutate(Year = 2023)
+spin23 <- read.csv("active-spin_2023.csv") %>% 
+  mutate(Year = 2023)
+
+
+# List of data frames
+df_list <- list(dashboard21 = dashboard21, dashboard22 = dashboard22, dashboard23 = dashboard23)
+
+# Variables you are interested in
+selected_vars <- c("First_Name", "Last_Name","Year" ,"Pitch_Type", "MPH", "RPM", "VAA", "Ext (ft)", "H-Rel Pt", "V-Mov", "H-Mov",
+                   "Usage.", "wOBA", "Pitches")
+
+# Initialize an empty list to store the new data frames
+selected_df_list <- list()
+
+for (name in names(df_list)) {
+  # Select the desired variables using select function from dplyr
+  assign(paste0(name, "_selected"), df_list[[name]] %>% select(all_of(selected_vars)))
+}
+
+#Combine all 3 years in single df
+dashboard <- rbind(dashboard21_selected, dashboard22_selected, dashboard23_selected)
+hard <- rbind(hard21, hard22, hard23)
+spin <- rbind(spin21, spin22, spin23)
+
+# Get adequate sample
 dashboard <- dashboard %>% 
   mutate(full_name = paste(First_Name, Last_Name)) %>% 
   select(full_name, everything()) %>% 
-  filter(Pitches >= 30 & Usage. >= .05)
+  filter(Pitches >= 100 & Usage. >= .10)
 
+#add full name variable
 hard <- hard %>% 
   mutate(full_name = paste(First_Name, Last_Name)) %>% 
-  select(full_name,Pitch_Type ,Hard.)
+  select(full_name,Pitch_Type ,Hard., Year)
 
-dashboard <- merge(dashboard, hard, by = c("full_name", "Pitch_Type"))
+#Get hard hit data into df
+dashboard <- merge(dashboard, hard, by = c("full_name", "Pitch_Type", "Year"))
 
-fb <- dashboard %>% 
-  filter(Pitch_Type == "FF")
+#Get gyro spin data
+spin <- spin %>% 
+  mutate(
+    full_name = str_split(last_name..first_name, ", ") %>%
+      map_chr(~ paste(.[2], .[1]))
+  ) %>% 
+  select(full_name, everything()) %>% 
+  select(-last_name..first_name)
 
-spin2$full_name <- paste(spin2$first_name, spin2$last_name)
-spin3$full_name <- paste(spin3$first_name, spin3$last_name)
-
-spin <- bind_rows(spin2, spin3) %>%
-  distinct(full_name, .keep_all = TRUE) %>% 
-  mutate(full_name = trimws(full_name))
-
-## Join Tables ##
-
+# Start FB Unique Analysis
 spinfb <- spin %>% 
-  select(full_name, active_spin_fourseam)
+  select(full_name, active_spin_fourseam, Year)
 
 fb <- dashboard %>% 
   filter(Pitch_Type == "FF")
 
-allfb <- merge(fb, spinfb, by = "full_name")
+allfb <- merge(fb, spinfb, by = c("full_name", "Year"))
 
-## Start Unique Analysis ##
-## Fastballs ##
-
+#Round Values
 allfb$MPH <- round(allfb$MPH,1)
 allfb$RPM <- round(allfb$RPM,0)
 allfb$VAA <- round(allfb$VAA, 1)
@@ -57,43 +90,28 @@ allfb$`V-Mov` <- round(allfb$`V-Mov`, 1)
 allfb$`H-Mov` <- abs(allfb$`H-Mov`)
 allfb$`H-Mov` <- round(allfb$`H-Mov`,1)
 
+# Select Unique Variables
 allfb <- allfb %>% 
-  select(full_name, Pitches,Pitch_Type ,MPH, RPM, active_spin_fourseam, `V-Mov`, `H-Mov`,`V-Rel Pt`,`Ext (ft)`, `H-Rel Pt`, wOBA, VAA, Usage., Hard.) %>% 
-  filter(`V-Rel Pt` >= 4.4) ## No submarines;
+  select(full_name, Pitches,Pitch_Type ,MPH, 
+         RPM, active_spin_fourseam, `V-Mov`, `H-Mov`,`Ext (ft)`, 
+         `H-Rel Pt`, wOBA, VAA, Usage., Hard., Year)
 
-# Normalize the Data #
-
+#Find uniqueness by each metric
 allfb$vert_norm <- abs((allfb$`V-Mov` - mean(allfb$`V-Mov`)) / sd(allfb$`V-Mov`)) 
 allfb$va_norm <- abs((allfb$VAA - mean(allfb$VAA)) / sd(allfb$VAA))
 allfb$rpm_norm <- abs((allfb$RPM - mean(allfb$RPM)) / sd(allfb$RPM))
 allfb$horiz_norm <- abs((allfb$`H-Mov` - mean(allfb$`H-Mov`)) / sd(allfb$`H-Mov`)) 
 allfb$gyro_norm <- abs((allfb$active_spin_fourseam - mean(allfb$active_spin_fourseam, na.rm =T)) / sd(allfb$active_spin_fourseam, na.rm =T))
-allfb$mph_norm <- abs((allfb$MPH - mean(allfb$MPH)) / sd(allfb$MPH))
+allfb$mph_norm <- (allfb$MPH - mean(allfb$MPH)) / sd(allfb$MPH)
 
-
-allfb$total <- sqrt(abs(allfb$vert_norm + allfb$horiz_norm + allfb$gyro_norm))
-allfb$total <- ifelse(allfb$MPH > 96, allfb$total + (allfb$mph_norm), allfb$total) # Boost for FB Above 96
-allfb$total <- ifelse(allfb$RPM >= 2200, allfb$total + (allfb$rpm_norm), allfb$total) # Boost for FB RPM above 2200
-allfb$total <- ifelse(allfb$VAA > -5, allfb$total + (allfb$va_norm), allfb$total) # Boost for flat VAA
-allfb$percentile <- cut(allfb$total, breaks = quantile(allfb$total, probs = seq(0, 1, by = 0.01), na.rm = TRUE), labels = FALSE)
-allfb$plus <- round((allfb$total / mean(allfb$total, na.rm = T))*100)
-
-allfb1 <- allfb %>% 
-  select(full_name,Pitch_Type ,Pitches, MPH, RPM, active_spin_fourseam, 
-         `V-Mov`, `H-Mov`, VAA, plus, wOBA, Usage., Hard.) %>% 
-  mutate(gyro = active_spin_fourseam) %>% 
-  select(-active_spin_fourseam)
-
-
-## Sinkers ##
-
+# Sinker Uniqueness
 si <- dashboard %>% 
   filter(Pitch_Type == "SI")
 
 spinsi <- spin %>% 
-  select(full_name, active_spin_sinker)
+  select(full_name, active_spin_sinker, Year)
 
-allsi <- merge(si, spinsi, by = "full_name")
+allsi <- merge(si, spinsi, by = c("full_name", "Year"))
 
 allsi$MPH <- round(allsi$MPH,1)
 allsi$RPM <- round(allsi$RPM,0)
@@ -105,38 +123,25 @@ allsi$`H-Mov` <- abs(allsi$`H-Mov`)
 allsi$`H-Mov` <- round(allsi$`H-Mov`,1)
 
 allsi <- allsi %>% 
-  select(full_name,Pitch_Type ,Pitches, MPH, RPM, active_spin_sinker, 
-         `V-Mov`, `H-Mov`,`V-Rel Pt`,`Ext (ft)`, `H-Rel Pt`, wOBA, VAA, Usage., Hard.) %>% 
-  filter(`V-Rel Pt` >= 4.4) ## No submarines; 
-
-# Normalize Data #
+  select(full_name, Pitches,Pitch_Type ,MPH, 
+         RPM, active_spin_sinker, `V-Mov`, `H-Mov`,`Ext (ft)`, 
+         `H-Rel Pt`, wOBA, VAA, Usage., Hard., Year)
 
 allsi$vert_norm <- abs((allsi$`V-Mov` - mean(allsi$`V-Mov`)) / sd(allsi$`V-Mov`)) 
 allsi$va_norm <- abs((allsi$VAA - mean(allsi$VAA)) / sd(allsi$VAA))
 allsi$rpm_norm <- abs((allsi$RPM - mean(allsi$RPM)) / sd(allsi$RPM))
 allsi$horiz_norm <- abs((allsi$`H-Mov` - mean(allsi$`H-Mov`)) / sd(allsi$`H-Mov`)) 
 allsi$gyro_norm <- abs((allsi$active_spin_sinker - mean(allsi$active_spin_sinker, na.rm =T)) / sd(allsi$active_spin_sinker, na.rm =T))
-allsi$mph_norm <- abs((allsi$MPH - mean(allsi$MPH)) / sd(allsi$MPH))
+allsi$mph_norm <- (allsi$MPH - mean(allsi$MPH)) / sd(allsi$MPH)
 
-allsi$total <- sqrt(abs(allsi$vert_norm + allsi$horiz_norm + allsi$gyro_norm + allsi$mph_norm))
-allsi$percentile <- cut(allsi$total, breaks = quantile(allsi$total, probs = seq(0, 1, by = 0.01), na.rm = TRUE), labels = FALSE)
-allsi$plus <- round((allsi$total / mean(allsi$total, na.rm = T))*100)
-
-allsi1 <- allsi %>% 
-  select(full_name,Pitch_Type ,Pitches, MPH, RPM, active_spin_sinker, 
-         `V-Mov`, `H-Mov`, VAA, plus, wOBA, Usage., Hard.) %>% 
-  mutate(gyro = active_spin_sinker) %>% 
-  select(-active_spin_sinker)
-
-## Cutters ##
-
+# Cutter Uniqueness
 cu <- dashboard %>% 
   filter(Pitch_Type == "FC")
 
 spincu <- spin %>% 
-  select(full_name, active_spin_cutter)
+  select(full_name, active_spin_cutter, Year)
 
-allcu <- merge(cu, spincu, by = "full_name")
+allcu <- merge(cu, spincu, by = c("full_name", "Year"))
 
 allcu$MPH <- round(allcu$MPH, 1)
 allcu$RPM <- round(allcu$RPM, 0)
@@ -149,37 +154,25 @@ allcu$`H-Mov` <- round(allcu$`H-Mov`, 1)
 
 
 allcu <- allcu %>%
-  select(full_name, Pitch_Type ,Pitches, MPH, RPM, active_spin_cutter, `V-Mov`, 
-         `H-Mov`, `V-Rel Pt`, `Ext (ft)`, `H-Rel Pt`, wOBA, VAA, Usage., Hard.) %>%
-  filter(`V-Rel Pt` >= 4.4) ## No submarines
+  select(full_name, Pitches,Pitch_Type ,MPH, 
+         RPM, active_spin_cutter, `V-Mov`, `H-Mov`,`Ext (ft)`, 
+         `H-Rel Pt`, wOBA, VAA, Usage., Hard., Year)
 
 allcu$vert_norm <- abs((allcu$`V-Mov` - mean(allcu$`V-Mov`)) / sd(allcu$`V-Mov`))
 allcu$va_norm <- abs((allcu$VAA - mean(allcu$VAA)) / sd(allcu$VAA))
 allcu$rpm_norm <- abs((allcu$RPM - mean(allcu$RPM)) / sd(allcu$RPM))
 allcu$horiz_norm <- abs((allcu$`H-Mov` - mean(allcu$`H-Mov`)) / sd(allcu$`H-Mov`))
 allcu$gyro_norm <- abs((allcu$active_spin_cutter - mean(allcu$active_spin_cutter, na.rm = TRUE)) / sd(allcu$active_spin_cutter, na.rm = TRUE))
-allcu$mph_norm <- abs((allcu$MPH - mean(allcu$MPH)) / sd(allcu$MPH))
+allcu$mph_norm <- (allcu$MPH - mean(allcu$MPH)) / sd(allcu$MPH)
 
-allcu$total <- sqrt(abs(allcu$horiz_norm + allcu$rpm_norm + allcu$vert_norm))
-allcu$total <- ifelse(allcu$MPH > 91, allcu$total + (allcu$mph_norm), allcu$total)
-allcu$total <- ifelse(allcu$VAA > -5.5, allcu$total + (allcu$va_norm), allcu$total)
-allcu$percentile <- cut(allcu$total, breaks = quantile(allcu$total, probs = seq(0, 1, by = 0.01), na.rm = TRUE), labels = FALSE)
-allcu$plus <- round((allcu$total / mean(allcu$total, na.rm = T))*100)
-
-allcu1 <- allcu %>% 
-  select(full_name, Pitch_Type ,Pitches, MPH, RPM, active_spin_cutter, 
-         `V-Mov`, `H-Mov`, VAA, plus, wOBA, Usage., Hard.) %>% 
-  mutate(gyro = active_spin_cutter) %>% 
-  select(-active_spin_cutter)
-
-## Slider ##
+#Slider Uniqueness
 sl <- dashboard %>% 
   filter(Pitch_Type %in% c("SW", "SL", "SV"))
 
 spinsl <- spin %>% 
-  select(full_name, active_spin_slider)
+  select(full_name, active_spin_slider, Year)
 
-allsl <- merge(sl, spinsl, by = "full_name")
+allsl <- merge(sl, spinsl, by = c("full_name", "Year"))
 
 allsl$MPH <- round(allsl$MPH, 1)
 allsl$RPM <- round(allsl$RPM, 0)
@@ -192,39 +185,24 @@ allsl$`H-Mov` <- round(allsl$`H-Mov`, 1)
 
 allsl <- allsl %>%
   select(full_name,Pitch_Type ,Pitches, MPH, RPM, active_spin_slider, 
-         `V-Mov`, `H-Mov`, `V-Rel Pt`, `Ext (ft)`, `H-Rel Pt`,
-         wOBA, VAA, Usage., Hard.) %>%
-  filter(`V-Rel Pt` >= 4.4) ## No submarines
+         `V-Mov`, `H-Mov`, `Ext (ft)`, `H-Rel Pt`,
+         wOBA, VAA, Usage., Hard., Year)
 
 allsl$vert_norm <- abs((allsl$`V-Mov` - mean(allsl$`V-Mov`)) / sd(allsl$`V-Mov`))
 allsl$va_norm <- abs((allsl$VAA - mean(allsl$VAA)) / sd(allsl$VAA))
 allsl$rpm_norm <- abs((allsl$RPM - mean(allsl$RPM)) / sd(allsl$RPM))
 allsl$horiz_norm <- abs((allsl$`H-Mov` - mean(allsl$`H-Mov`)) / sd(allsl$`H-Mov`))
-allsl$gyro_norm <- abs((allsl$active_spin_slider - mean(allsl$active_spin_slider, na.rm = TRUE)) / sd(allsl$active_spin_sliders, na.rm = TRUE))
+allsl$gyro_norm <- abs((allsl$active_spin_slider - mean(allsl$active_spin_slider, na.rm = TRUE)) / sd(allsl$active_spin_slider, na.rm = TRUE))
 allsl$mph_norm <- abs((allsl$MPH - mean(allsl$MPH)) / sd(allsl$MPH))
 
-mean(allsl$`H-Mov`)
-
-allsl$total <- sqrt(abs(allsl$vert_norm + allsl$horiz_norm + allsl$rpm_norm + allsl$mph_norm))
-allsl$total <- ifelse(allsl$RPM >= 2480, allsl$total + allsl$rpm_norm, allsl$total)
-allsl$percentile <- cut(allsl$total, breaks = quantile(allsl$total, probs = seq(0, 1, by = 0.01), na.rm = TRUE), labels = FALSE)
-allsl$plus <- round((allsl$total / mean(allsl$total, na.rm = TRUE)) * 100)
-
-allsl1 <- allsl %>% 
-  select(full_name,Pitch_Type ,Pitches, MPH, RPM, active_spin_slider, 
-         `V-Mov`, `H-Mov`, VAA, plus, wOBA, Usage., Hard.) %>% 
-  mutate(gyro = active_spin_slider) %>% 
-  select(-active_spin_slider)
-
-## Changeup ##
-
+# Changeup Uniqueness
 ch <- dashboard %>% 
   filter(Pitch_Type == "CH")
 
 spinch <- spin %>% 
-  select(full_name, active_spin_changeup)
+  select(full_name, active_spin_changeup, Year)
 
-allch <- merge(ch, spinch, by = "full_name")
+allch <- merge(ch, spinch, by = c("full_name", "Year"))
 
 allch$MPH <- round(allch$MPH, 1)
 allch$RPM <- round(allch$RPM, 0)
@@ -237,9 +215,8 @@ allch$`H-Mov` <- round(allch$`H-Mov`, 1)
 
 allch <- allch %>%
   select(full_name,Pitch_Type ,Pitches, MPH, RPM, active_spin_changeup, 
-         `V-Mov`, `H-Mov`, `V-Rel Pt`, `Ext (ft)`, `H-Rel Pt`, 
-         wOBA, VAA, Usage., Hard.) %>%
-  filter(`V-Rel Pt` >= 4.4) ## No submarines
+         `V-Mov`, `H-Mov`, `Ext (ft)`, `H-Rel Pt`, 
+         wOBA, VAA, Usage., Hard., Year)
 
 allch$vert_norm <- abs((allch$`V-Mov` - mean(allch$`V-Mov`)) / sd(allch$`V-Mov`))
 allch$va_norm <- abs((allch$VAA - mean(allch$VAA)) / sd(allch$VAA))
@@ -248,27 +225,16 @@ allch$horiz_norm <- abs((allch$`H-Mov` - mean(allch$`H-Mov`)) / sd(allch$`H-Mov`
 allch$gyro_norm <- abs((allch$active_spin_changeup - mean(allch$active_spin_changeup, na.rm = TRUE)) / sd(allch$active_spin_changeup, na.rm = TRUE))
 allch$mph_norm <- abs((allch$MPH - mean(allch$MPH)) / sd(allch$MPH))
 
-allch$total <- sqrt(abs(allch$vert_norm + allch$gyro_norm + allch$horiz_norm + allch$mph_norm))
-allch$total <- ifelse(allch$RPM > 2000, allch$total + allch$rpm_norm, allch$total)
-allch$percentile <- cut(allch$total, breaks = quantile(allch$total, probs = seq(0, 1, by = 0.01), na.rm = TRUE), labels = FALSE)
-allch$plus <- round((allch$total / mean(allch$total, na.rm = TRUE)) * 100)
-
-allch1 <- allch %>% 
-  select(full_name,Pitch_Type ,Pitches, MPH, RPM, active_spin_changeup, 
-         `V-Mov`, `H-Mov`, VAA, plus, wOBA, Usage., Hard.) %>% 
-  mutate(gyro = active_spin_changeup) %>% 
-  select(-active_spin_changeup)
-
-## Curveballs ##
+#Curveball Uniqueness
 
 cb <- dashboard %>% 
   filter(Pitch_Type == "CU")
 
 spincb <- spin %>% 
-  select(full_name, active_spin_curve)
+  select(full_name, active_spin_curve, Year)
 
-allcb <- merge(cb, spincb, by = "full_name")  
-  
+allcb <- merge(cb, spincb, by = c("full_name", "Year"))
+
 allcb$MPH <- round(allcb$MPH, 1)
 allcb$RPM <- round(allcb$RPM, 0)
 allcb$VAA <- round(allcb$VAA, 1)
@@ -280,9 +246,8 @@ allcb$`H-Mov` <- round(allcb$`H-Mov`, 1)
 
 allcb <- allcb %>%
   select(full_name, Pitch_Type ,Pitches, MPH, RPM, active_spin_curve, 
-         `V-Mov`, `H-Mov`, `V-Rel Pt`, `Ext (ft)`, `H-Rel Pt`
-         , wOBA, VAA, Usage., Hard.) %>%
-  filter(`V-Rel Pt` >= 4.4) ## No submarines
+         `V-Mov`, `H-Mov`, `Ext (ft)`, `H-Rel Pt`
+         , wOBA, VAA, Usage., Hard., Year)
 
 allcb$vert_norm <- abs((allcb$`V-Mov` - mean(allcb$`V-Mov`)) / sd(allcb$`V-Mov`))
 allcb$va_norm <- abs((allcb$VAA - mean(allcb$VAA)) / sd(allcb$VAA))
@@ -291,19 +256,7 @@ allcb$horiz_norm <- abs((allcb$`H-Mov` - mean(allcb$`H-Mov`)) / sd(allcb$`H-Mov`
 allcb$gyro_norm <- abs((allcb$active_spin_curve - mean(allcb$active_spin_curve, na.rm = TRUE)) / sd(allcb$active_spin_curve, na.rm = TRUE))
 allcb$mph_norm <- abs((allcb$MPH - mean(allcb$MPH)) / sd(allcb$MPH))
 
-
-allcb$total <- sqrt(abs(allcb$vert_norm + allcb$mph_norm + allcb$horiz_norm + allcb$rpm_norm))
-allcb$percentile <- cut(allcb$total, breaks = quantile(allcb$total, probs = seq(0, 1, by = 0.01), na.rm = TRUE), labels = FALSE)
-allcb$plus <- round((allcb$total / mean(allcb$total, na.rm = TRUE)) * 100)
-
-allcb1 <- allcb %>% 
-  select(full_name, Pitch_Type ,Pitches, MPH, RPM, active_spin_curve, 
-         `V-Mov`, `H-Mov`, VAA, plus, wOBA, Usage., Hard.) %>% 
-  mutate(gyro = active_spin_curve) %>% 
-  select(-active_spin_curve)
-
-## Splitter ##
-
+# Splitter Uniqueness
 sp <- dashboard %>% 
   filter(Pitch_Type == "FS")
 
@@ -320,75 +273,61 @@ allsp$`H-Mov` <- round(allsp$`H-Mov`, 1)
 
 allsp <- sp %>%
   select(full_name,Pitch_Type,Pitches, MPH, RPM, `V-Mov`, 
-         `H-Mov`, `V-Rel Pt`, `Ext (ft)`, 
-         `H-Rel Pt`, wOBA, VAA, Usage., Hard.) %>%
-  filter(`V-Rel Pt` >= 4.4)
+         `H-Mov`, `Ext (ft)`, 
+         `H-Rel Pt`, wOBA, VAA, Usage., Hard., Year)
 
 allsp$vert_norm <- abs((allsp$`V-Mov` - mean(allsp$`V-Mov`)) / sd(allsp$`V-Mov`))
 allsp$va_norm <- abs((allsp$VAA - mean(allsp$VAA)) / sd(allsp$VAA))
 allsp$rpm_norm <- abs((allsp$RPM - mean(allsp$RPM)) / sd(allsp$RPM))
 allsp$horiz_norm <- abs((allsp$`H-Mov` - mean(allsp$`H-Mov`)) / sd(allsp$`H-Mov`))
 allsp$mph_norm <- abs((allsp$MPH - mean(allsp$MPH)) / sd(allsp$MPH))
+allsp$gyro_norm <- .5
 
-allsp$total <- sqrt(abs(allsp$vert_norm + allsp$horiz_norm))
-allsp$total <- ifelse(allsp$MPH >= 88, allsp$mph_norm + allsp$total, allsp$total)
-allsp$total <- ifelse(allsp$RPM <= 1400, allsp$total + allsp$rpm_norm, allsp$total)
-allsp$percentile <- cut(allsp$total, breaks = quantile(allsp$total, probs = seq(0, 1, by = 0.01), na.rm = TRUE), labels = FALSE)
-allsp$plus <- round((allsp$total / mean(allsp$total, na.rm = TRUE)) * 100)
 
-allsp1 <- allsp %>% 
-  select(full_name, Pitch_Type ,Pitches, MPH, RPM, `V-Mov`, `H-Mov`, VAA, plus, wOBA, Usage., Hard.) %>% 
-  mutate(gyro = NA)
+# List of data frames
+df_list2 <- list(allcb = allcb, allch = allch, allcu = allcu,
+                allfb = allfb, allsi = allsi, allsl = allsl, allsp = allsp)
 
-# Bind Tables Together #
+# Variables you are interested in
+selected_vars2 <- c("full_name", "Pitch_Type", "Year", "wOBA", "Hard.", "vert_norm", 
+                   "va_norm", "rpm_norm", "horiz_norm", "gyro_norm", "mph_norm")
 
-joined_table <- rbind(allcb1, allfb1, allch1, allsl1, allcu1, allsi1, allsp1)
+# Initialize an empty list to store the new data frames
+selected_df_list2 <- list()
 
-joined_table <- joined_table %>% 
-  arrange(desc(plus))
+for (name in names(df_list2)) {
+  # Select the desired variables using select function from dplyr
+  assign(paste0(name, "_selected"), df_list2[[name]] %>% select(all_of(selected_vars2)))
+}
 
-joined_table$MPH <- round(joined_table$MPH, 1)
-joined_table$RPM <- round(joined_table$RPM, 0)
-joined_table$`V-Mov` <- round(joined_table$`V-Mov`, 2)
-joined_table$`H-Mov` <- round(joined_table$`H-Mov`, 2)
-joined_table$VAA <- round(joined_table$VAA, 2)
+# Use dataframe for ML
+all <- rbind(allcb_selected, allch_selected, allcu_selected, allfb_selected, allsi_selected,
+             allsl_selected, allsp_selected)
 
+# Create Handeness Variable
 dashboard$`H-Mov` <- dashboard$`H-Mov`*-1 ## Adjust statcast horizontal movement numbers
-
-# Create handeness Variable #
 
 rhp <- dashboard %>%
   mutate(hand = ifelse(Pitch_Type %in% c("SL", "SW", "SW") & `H-Mov` < 0, "RHP",
                        ifelse(Pitch_Type == "SI" & `H-Mov` > 0, "RHP",
                               ifelse(Pitch_Type == "CU" & `H-Mov` < 0, "RHP",
                                      ifelse(Pitch_Type == "CH" & `H-Mov` > 0, "RHP", "LHP")
-                                     )
                               )
                        )
+  )
   ) %>%
   select(full_name, hand) %>%
   group_by(full_name) %>%
   summarise(hand = max(hand))
 
 
-
-
-joined_table <- merge(joined_table, rhp, by = "full_name")
+rhp$hand[rhp$full_name == "Drew Smyly"] <- "LHP"
 dashboard <- merge(dashboard, rhp, by = "full_name")
 
-# Create Average Movement for Shiny Graph #
-
+# Get average movements for plot
 avg_movements <- dashboard %>%
   group_by(hand, Pitch_Type) %>%
   summarise(avg_hmov = mean(`H-Mov`),
             avg_vmov = mean(`V-Mov`)) %>% 
   filter(!Pitch_Type %in% c("EP", "Enyel", "Yerry", "s", "FA", "FO", "SC")) %>% 
   na.omit()
-
-# Save Files #
-
-setwd("/Users/aidanbeilke/Desktop/Shiny App")
-
-write.xlsx(joined_table, file = "joined_table.xlsx")
-write.xlsx(dashboard, file = "dashboard.xlsx")
-write.xlsx(avg_movements, file = "avg_movements.xlsx")
